7a4cc3e451677b60e6bc2617363370e9ce9dc771
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-04-07 20:35:42 +0200

    initializing project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '97f8e6642df22e862f970ff42167a739'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '91d4df69-84b5-41df-b122-915a5893ca66'
+};
